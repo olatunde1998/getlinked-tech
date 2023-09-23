83ef915229cc90ab97935e194351f82d7f981b87
geodevcode <geodevinstitute@gmail.com>
2023-09-23 21:13:01 +0100

    color added to timeline heading

--- src/app/components/LandPageComponents/TailwindTimeline.tsx
+++ src/app/components/LandPageComponents/TailwindTimeline.tsx
@@ -15,24 +15,24 @@ export const TailwindTimeLine = () => {
         </div>
         <div className="timeline">
           <div className="timeline__component timeline__component--bg text-right">
-            <h2 className="timeline__title ">Hackathon Announcement</h2>
+            <h2 className="timeline__title text-[#D434FE]">Hackathon Announcement</h2>
             <p className="timeline__paragraph max-w-[500px] ml-auto text-[14px] mr-0">
               The getlinked tech hackathon 1.0 is formally announced to the
               general public and teams begin to get ready to registe
             </p>
           </div>
-          <div className="timeline__middle my-2 my-2">
+          <div className="timeline__middle my-2">
             <div className="timeline__point text-center text-white flex items-center justify-center">
               1
             </div>
           </div>
           <div className="timeline__component">
-            <div className="timeline__date timeline__date--right">
+            <div className="text-[#D434FE] timeline__date timeline__date--right">
               November 18, 2023
             </div>
           </div>
           <div className="timeline__component text-right">
-            <div className="timeline__date">February 25, 2019</div>
+            <div className="text-[#D434FE] timeline__date">February 25, 2019</div>
           </div>
           <div className="timeline__middle my-2">
             <div className="timeline__point text-center text-white flex items-center justify-center">
@@ -40,14 +40,14 @@ export const TailwindTimeLine = () => {
             </div>
           </div>
           <div className="timeline__component timeline__component--bg">
-            <h2 className="timeline__title">Teams Registration begins</h2>
+            <h2 className="text-[#D434FE] timeline__title">Teams Registration begins</h2>
             <p className="timeline__paragraph max-w-[500px] mr-auto text-[14px] ml-0">
               Interested teams can now show their interest in the getlinked tech
               hackathon 1.0 2023 by proceeding to register
             </p>
           </div>
           <div className="timeline__component timeline__component--bg text-right">
-            <h2 className="timeline__title">Teams Registration ends</h2>
+            <h2 className=" text-[#D434FE] timeline__title">Teams Registration ends</h2>
             <p className="timeline__paragraph max-w-[500px] ml-auto text-[14px] mr-0">
               Interested Participants are no longer Allowed to register
             </p>
@@ -58,12 +58,12 @@ export const TailwindTimeLine = () => {
             </div>
           </div>
           <div className="timeline__component">
-            <div className="timeline__date timeline__date--right">
+            <div className="text-[#D434FE] timeline__date timeline__date--right">
               November 18, 2023
             </div>
           </div>
           <div className="timeline__component text-right">
-            <div className="timeline__date timeline__date--right">
+            <div className="text-[#D434FE] timeline__date timeline__date--right">
               November 18, 2023
             </div>
           </div>
@@ -73,7 +73,7 @@ export const TailwindTimeLine = () => {
             </div>
           </div>
           <div className="timeline__component timeline__component--bg">
-            <h2 className="timeline__title">
+            <h2 className="text-[#D434FE] timeline__title">
               Announcement of the accepted teams and ideas
             </h2>
             <p className="timeline__paragraph max-w-[500px] mr-auto text-[14px] ml-0">
@@ -83,7 +83,7 @@ export const TailwindTimeLine = () => {
           </div>
 
           <div className="timeline__component timeline__component--bg text-right">
-            <h2 className="timeline__title">Teams Registration ends</h2>
+            <h2 className="text-[#D434FE] timeline__title">Teams Registration ends</h2>
             <p className="timeline__paragraph max-w-[500px] ml-auto text-[14px] mr-0">
               Interested Participants are no longer Allowed to register
             </p>
@@ -94,12 +94,12 @@ export const TailwindTimeLine = () => {
             </div>
           </div>
           <div className="timeline__component ">
-            <div className="timeline__date timeline__date--right">
+            <div className="text-[#D434FE] timeline__date timeline__date--right">
               November 18, 2023
             </div>
           </div>
           <div className="timeline__component timeline__component--bottom pt-32 text-right">
-            <div className="timeline__date">November 18, 2023</div>
+            <div className="text-[#D434FE] timeline__date">November 18, 2023</div>
           </div>
           <div className="timeline__middle">
             <div className="timeline__point timeline__point--bottom text-center text-white flex items-center justify-center">
@@ -107,7 +107,7 @@ export const TailwindTimeLine = () => {
             </div>
           </div>
           <div className="timeline__component timeline__component--bottom timeline__component--bg pt-32">
-            <h2 className="timeline__title">Demo Day</h2>
+            <h2 className="text-[#D434FE] timeline__title">Demo Day</h2>
             <p className="timeline__paragraph max-w-[500px] mr-auto text-[14px] ml-0">
               Teams get the opportunity to pitch their projects to judges. The
               winner of the hackathon will also be announced on this day
